f3d83cd3ad724c1192f9af3d73c9a1210f826dfc
Krzysztof Potocki <kpotocki89@gmail.com>
2022-10-11 15:57:30 +0200

    Fix port

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.use((req, res) => {
   res.status(404).send({ message: "Not found..." });
 });
 
-const server = app.listen("8000", () => {
-  console.log("Server is running on port: 8000");
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log("Server is running...");
 });
